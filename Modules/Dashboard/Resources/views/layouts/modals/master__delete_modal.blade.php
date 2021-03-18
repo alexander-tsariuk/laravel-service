54ce090dd8094e981499c89db4d03ffa0ebe6c9c
alexander.tsariuk@icloud.com <alexander.tsariuk@icloud.com>
2021-03-18 15:48:46 +0200

    language module

---
+++ Modules/Dashboard/Resources/views/layouts/modals/master__delete_modal.blade.php
@@ -0,0 +1,19 @@
+<div class="modal fade" id="modal-delete-item">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Удаление записи</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Вы уверены, что хотите удалить выбранный элемент?</p>
+            </div>
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Нет</button>
+                <button type="button" class="btn btn-primary">Да</button>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ Modules/Language/Resources/views/create.blade.php
@@ -0,0 +1,69 @@
+@extends('dashboard::layouts.master')
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <form id="editItem" action="{{ route('dashboard.language.update', ['itemId' => $item->id]) }}" method="POST">
+                @method('PUT')
+                @csrf
+                <div class="card">
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label for="name">Название языковой версии</label>
+                            <input
+                                type="text"
+                                class="form-control form-control-border {{ $errors->has('name1') ? 'is-invalid' : '' }}"
+                                name="name"
+                                placeholder="English"
+                                value="{{ old('name') ?? $item->name }}"
+                            />
+                            @if($errors->has('name1'))
+                                <span class="text-danger">{{ $errors->first('name1') }}</span>
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <label for="prefix">Префикс <br><code>Только латинские символы</code></label>
+                            <input
+                                type="text"
+                                class="form-control form-control-border border-width-2 {{ $errors->has('name') ? 'is-invalid' : '' }}"
+                                name="prefix"
+                                placeholder="en"
+                                value="{{ old('prefix') ?? $item->prefix }}"
+                            />
+                            @if($errors->has('prefix'))
+                                <span class="text-danger">{{ $errors->first('prefix') }}</span>
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <label for="status">Статус</label>
+                            <select class="custom-select form-control-border {{ $errors->has('status') ? 'is-invalid' : '' }}" name="status">
+                                <option value="0" {{ !$item->status ? 'selected' : '' }}>Неактивен</option>
+                                <option value="1" {{ $item->status == 1 ? 'selected' : '' }}>Активен</option>
+                            </select>
+                            @if($errors->has('status'))
+                                <span class="text-danger">{{ $errors->first('status') }}</span>
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <label for="default">По-умолчанию <br><code>При установке этого параметра "Да", языковая версия станет по-умолчанию.</code></label>
+                            <select class="custom-select form-control-border {{ $errors->has('default') ? 'is-invalid' : '' }}" name="default">
+                                <option value="0" {{ !$item->default ? 'selected' : '' }}>Нет</option>
+                                <option value="1" {{ $item->default == 1 ? 'selected' : '' }}>Да</option>
+                            </select>
+                            @if($errors->has('default'))
+                                <span class="text-danger">{{ $errors->first('default') }}</span>
+                            @endif
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-info">Сохранить</button>
+                        <a href="{{ route('dashboard.language.index') }}" class="btn btn-default float-right">Отмена</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    @if(isset($errors->ha))
+@endsection
